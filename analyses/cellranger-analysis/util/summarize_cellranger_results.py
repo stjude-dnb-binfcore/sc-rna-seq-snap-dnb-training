22226734342df87793a20b9196751e2918923d2a
Antonia Chroni <46328680+AntoniaChroni@users.noreply.github.com>
2024-10-24 15:21:18 -0500

    Update analyses/cellranger-analysis/util/summarize_cellranger_results.py
    Co-authored-by: Sharon Freshour <42744113+sharonfreshour@users.noreply.github.com>

--- analyses/cellranger-analysis/util/summarize_cellranger_results.py
+++ analyses/cellranger-analysis/util/summarize_cellranger_results.py
@@ -210,7 +210,7 @@ for filename in glob.glob(os.path.join(args.dir, "*", "outs", "metrics_summary.c
         if df.iloc[0]["GRCh38 Fraction Reads in Cells"] < 70:
             MajorWarnings = MajorWarnings + "GRCh38 Fraction Reads in Cells < 70%, "
             TotalWarnings += 1
-    elif args.genome == "mm10":
+    elif args.genome == "GRCm39":
         if df.iloc[0]["GRCm39 Fraction Reads in Cells"] < 70:
             MajorWarnings = MajorWarnings + "GRCm39 Fraction Reads in Cells < 70%, "
             TotalWarnings += 1

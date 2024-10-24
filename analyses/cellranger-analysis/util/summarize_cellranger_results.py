72426ecf84df686b1455811a790e389470ccbca4
Antonia Chroni <46328680+AntoniaChroni@users.noreply.github.com>
2024-10-24 15:15:55 -0500

    Update analyses/cellranger-analysis/util/summarize_cellranger_results.py
    Co-authored-by: Sharon Freshour <42744113+sharonfreshour@users.noreply.github.com>

--- analyses/cellranger-analysis/util/summarize_cellranger_results.py
+++ analyses/cellranger-analysis/util/summarize_cellranger_results.py
@@ -57,7 +57,7 @@ for filename in glob.glob(os.path.join(args.dir, "*", "outs", "metrics_summary.c
         if df.iloc[0]["GRCh38 Estimated Number of Cell Partitions"] < 500:
             Warnings = Warnings + "GRCh38 Estimated Number of Cell Partitions < 500, "
             TotalWarnings += 1
-    elif args.genome == "mm10":
+    elif args.genome == "GRCm39":
         if df.iloc[0]["mm10 Estimated Number of Cell Partitions"] < 500:
             Warnings = Warnings + "mm10 Estimated Number of Cell Partitions < 500, "
             TotalWarnings += 1

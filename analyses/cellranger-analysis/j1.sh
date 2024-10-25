1f2b09defa995a0f5e1287c95f9e1978413e4ce0
AntoniaChroni <achroni@noderome107.cm.cluster>
2024-10-25 14:51:14 -0500

    Replace genome referenc with mouse

--- analyses/cellranger-analysis/j1.sh
+++ analyses/cellranger-analysis/j1.sh
@@ -14,13 +14,13 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # If your `project_metadata` is not in `*.txt` file format
 # use the following code line to convert it
-cat ./mouse-test-dataset/project_metadata.tsv | sed 's/,/\t/g' > ./input/project_metadata.txt
+cat ./project_metadata.tsv | sed 's/,/\t/g' > ./input/project_metadata.txt
 
 
 ########################################################################
 # Run CellRanger for all libraries
 python ./util/run_cellranger.py --file=./input/project_metadata.txt \
-                                --transcriptome=GRCh38 \
+                                --transcriptome=GRCm39 \
                                 --create_bam=true \
                                 --output_dir=./results/02_cellranger_count/DefaultParameters/
                                 # --force_cells=8000

f25cde12109dbaf8530a5efbd3ce00db8c9859a5
AntoniaChroni <achroni@noderome225.cm.cluster>
2024-11-20 10:11:45 -0600

    Update with exec singularity

--- run-container/run-test-packages.sh
+++ run-container/run-test-packages.sh
@@ -6,9 +6,15 @@ set -o pipefail
 # set up running directory
 cd "$(dirname "${BASH_SOURCE[0]}")" 
 
+########################################################################
+# Read container path and file
+containerdir=$(realpath "$(dirname "${BASH_SOURCE[0]}")/..")
+echo "$containerdir"
+
 ################################################################################################################
 # Run module
-Rscript -e "rmarkdown::render('run-test-packages.Rmd', clean = FALSE,
+singularity exec ${containerdir}/rstudio_4.4.0_seurat_4.4.0_latest.sif \
+            Rscript -e "rmarkdown::render('run-test-packages.Rmd', clean = FALSE,
                               output_dir = file.path('.'),
                               output_file = c(paste('Report-', 'run-test-packages', '-', Sys.Date(), sep = '')),
                               output_format = 'all')"

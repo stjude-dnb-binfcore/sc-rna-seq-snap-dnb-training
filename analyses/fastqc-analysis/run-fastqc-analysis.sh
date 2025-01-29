d46a897f739bb04fcddc098f4d2f89a8953ac5d7
AntoniaChroni <achroni@noderome182.cm.cluster>
2025-01-29 10:02:56 -0600

    Update to accomodate multiple filepaths to fatsq

--- analyses/fastqc-analysis/run-fastqc-analysis.sh
+++ analyses/fastqc-analysis/run-fastqc-analysis.sh
@@ -8,17 +8,38 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 ########################################################################
 # Read multiple values and assign them to variables by parsing yaml file
-fastqc_dir=$(cat ../../project_parameters.Config.yaml | grep 'fastqc_dir:' | awk '{print $2}')
-fastqc_dir=${fastqc_dir//\"/}  # Removes all double quotes
+mapfile -t fastqc_dir < <(grep '^ *-' ../../project_parameters.Config.yaml | sed 's/^ *- *//')
+
+##################
+# Check if the variable is an array by testing if it has elements
+if [ "${#fastqc_dir[@]}" -gt 0 ]; then
+  echo "fastqc_dir is an array with ${#fastqc_dir[@]} elements."
+else
+  echo "fastqc_dir is not an array or is empty."
+fi
+
+##################
 
 # Create results dir
 mkdir -p results
 mkdir -p results/01-fastqc-reports
 
+
+################################################################################################################
+###### STEP 1 ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### 
 ################################################################################################################
 # Run FastQC per library
-fastqc -o results/01-fastqc-reports ${fastqc_dir}/*/*R2*.fastq.gz
 
+# Loop through the array and echo each element
+#for dir in "${fastqc_dir_array[@]}"; 
+for dir in $(echo "${fastqc_dir[@]}" | tr ' ' '\n' | sort); 
+  do
+  echo "Processing directory: ${dir}"
+  fastqc -o results/01-fastqc-reports ${dir}/*/*R2*.fastq.gz
+done
+
+################################################################################################################
+###### STEP 2 ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### 
 ################################################################################################################
 # Run multiqc for all samples in the `results` dir
 # to summarize results
@@ -32,3 +53,7 @@ mv multiqc_data 02-multiqc-reports
 # move the files related to multiqc in the main `results` dir
 mv 02-multiqc-reports ../
 mv multiqc_report.html ../
+
+################################################################################################################
+###### THE END ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### ###### 
+################################################################################################################

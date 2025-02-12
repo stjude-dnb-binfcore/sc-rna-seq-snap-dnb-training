ad1cfe0a9dc784e97127f0c34e731c416e854156
AntoniaChroni <achroni@noderome213.cm.cluster>
2025-02-11 18:21:53 -0600

    Update bash to run and merge only SingleR

--- analyses/cell-types-annotation/run-cell-types-annotation.sh
+++ analyses/cell-types-annotation/run-cell-types-annotation.sh
@@ -8,6 +8,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 ################################################################################################################
 # Run module
 Rscript --vanilla run-cell-types-annotation-SingleR.R
-Rscript --vanilla run-cell-types-annotation-gene-markers.R
+#Rscript --vanilla run-cell-types-annotation-gene-markers.R
 Rscript --vanilla 04-merge-cell-types-annotations-all.R
 ################################################################################################################

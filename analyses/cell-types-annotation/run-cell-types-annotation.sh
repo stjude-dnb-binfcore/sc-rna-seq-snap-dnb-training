a6c2c3a127a359f42a138a1ed3096e498250f099
AntoniaChroni <achroni@noderome230.cm.cluster>
2025-02-13 18:01:06 -0600

    Clean script3 and bash

--- analyses/cell-types-annotation/run-cell-types-annotation.sh
+++ analyses/cell-types-annotation/run-cell-types-annotation.sh
@@ -6,8 +6,15 @@ set -o pipefail
 # set up running directory
 cd "$(dirname "${BASH_SOURCE[0]}")" 
 
+# Download R scripts for dittoSeq library
+# This needs to be done only if the user wants to run `run-cell-types-annotation-gene-markers.R` (first time).
+cd util
+git clone https://github.com/dtm2451/dittoSeq.git
+
 ################################################################################################################
 # Run module
+cd "$(dirname "${BASH_SOURCE[0]}")" 
+
 #Rscript --vanilla run-cell-types-annotation-SingleR.R
 Rscript --vanilla run-cell-types-annotation-gene-markers.R
 #Rscript --vanilla 04-merge-cell-types-annotations-all.R

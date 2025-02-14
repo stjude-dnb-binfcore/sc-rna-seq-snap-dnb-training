1fa7781706269458a9786c9ba01bf6192cbf6bdf
AntoniaChroni <achroni@noderome230.cm.cluster>
2025-02-13 18:48:55 -0600

    explain about getGenes function and rm it

--- analyses/cell-types-annotation/run-cell-types-annotation.sh
+++ analyses/cell-types-annotation/run-cell-types-annotation.sh
@@ -6,10 +6,11 @@ set -o pipefail
 # set up running directory
 cd "$(dirname "${BASH_SOURCE[0]}")" 
 
+# This is NOT needed if we don't use the getGenes function in the `.util/function-calculate-cell-type-signature.R` 
 # Download R scripts for dittoSeq library
 # This needs to be done only if the user wants to run `run-cell-types-annotation-gene-markers.R` (first time).
-cd util
-git clone https://github.com/dtm2451/dittoSeq.git
+# cd util
+# git clone https://github.com/dtm2451/dittoSeq.git
 
 ################################################################################################################
 # Run module

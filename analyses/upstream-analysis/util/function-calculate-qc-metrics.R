13a8495a6ce65fffe090aa82e8b7b189cbf82662
AntoniaChroni <achroni@noderome178.cm.cluster>
2025-03-07 10:30:33 -0600

    Add line for mtdna vln plots

--- analyses/upstream-analysis/util/function-calculate-qc-metrics.R
+++ analyses/upstream-analysis/util/function-calculate-qc-metrics.R
@@ -71,6 +71,7 @@ create_vln_plot <- function(seurat_obj, grouping, min_genes, min_count, title_na
     theme_Publication(base_size = 11) + 
     xlab("") + 
     ylab("") + 
+    geom_hline(yintercept = mtDNA_pct_default, color = min_color_df$hex_codes, size = 1) +
     scale_fill_manual(values = alpha(c(vln_plot_color_df$hex_codes), .4)) +
     annotate(geom = "text", size = 2.5, x = 1, y = mtDNA_pct_default, label = glue::glue("Min mtDNA: {mtDNA_pct_default}"), color = min_color_df$hex_codes)
   
